25d3ebbd8e121acebe5979c97d60f95e27d864f6
FailCake <edu444@gmail.com>
2017-08-27 00:21:25 +0100

    Improved first chapter
    Signed-off-by: FailCake <edu444@gmail.com>

--- src/game/server/EnvFade.cpp
+++ src/game/server/EnvFade.cpp
@@ -1,4 +1,4 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+﻿//========= Copyright � 1996-2005, Valve Corporation, All rights reserved. ============//
 //
 // Purpose: Implements visual effects entities: sprites, beams, bubbles, etc.
 //
@@ -7,6 +7,10 @@
 
 #include "cbase.h"
 #include "shake.h"
+#ifdef INFESTED_DLL
+#include "asw_marine.h"
+#include "asw_player.h"
+#endif
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
@@ -17,38 +21,42 @@ private:
 
 	float m_Duration;
 	float m_HoldTime;
+	float m_ReverseDuration;
 
 	COutputEvent m_OnBeginFade;
 
 	DECLARE_DATADESC();
 
 public:
-	DECLARE_CLASS( CEnvFade, CLogicalEntity );
+	DECLARE_CLASS(CEnvFade, CLogicalEntity);
 
-	virtual void Spawn( void );
+	virtual void Spawn(void);
 
-	inline float Duration( void ) { return m_Duration; }
-	inline float HoldTime( void ) { return m_HoldTime; }
+	inline float Duration(void) { return m_Duration; }
+	inline float HoldTime(void) { return m_HoldTime; }
 
-	inline void SetDuration( float duration ) { m_Duration = duration; }
-	inline void SetHoldTime( float hold ) { m_HoldTime = hold; }
+	inline void SetDuration(float duration) { m_Duration = duration; }
+	inline void SetHoldTime(float hold) { m_HoldTime = hold; }
 
 	int DrawDebugTextOverlays(void);
 
 	// Inputs
-	void InputFade( inputdata_t &inputdata );
+	void InputFade(inputdata_t &inputdata);
+	void InputFadeReverse(inputdata_t &inputdata);
 };
 
-LINK_ENTITY_TO_CLASS( env_fade, CEnvFade );
+LINK_ENTITY_TO_CLASS(env_fade, CEnvFade);
 
-BEGIN_DATADESC( CEnvFade )
+BEGIN_DATADESC(CEnvFade)
 
-	DEFINE_KEYFIELD( m_Duration, FIELD_FLOAT, "duration" ),
-	DEFINE_KEYFIELD( m_HoldTime, FIELD_FLOAT, "holdtime" ),
+DEFINE_KEYFIELD(m_Duration, FIELD_FLOAT, "duration"),
+DEFINE_KEYFIELD(m_HoldTime, FIELD_FLOAT, "holdtime"),
+DEFINE_KEYFIELD(m_ReverseDuration, FIELD_FLOAT, "ReverseFadeDuration"),
 
-	DEFINE_INPUTFUNC( FIELD_VOID, "Fade", InputFade ),
+DEFINE_INPUTFUNC(FIELD_VOID, "Fade", InputFade),
+DEFINE_INPUTFUNC(FIELD_VOID, "FadeReverse", InputFadeReverse),
 
-	DEFINE_OUTPUT( m_OnBeginFade, "OnBeginFade"),
+DEFINE_OUTPUT(m_OnBeginFade, "OnBeginFade"),
 
 END_DATADESC()
 
@@ -62,19 +70,18 @@ END_DATADESC()
 //-----------------------------------------------------------------------------
 // Purpose: 
 //-----------------------------------------------------------------------------
-void CEnvFade::Spawn( void )
+void CEnvFade::Spawn(void)
 {
 }
-
 
 //-----------------------------------------------------------------------------
 // Purpose: Input handler that does the screen fade.
 //-----------------------------------------------------------------------------
-void CEnvFade::InputFade( inputdata_t &inputdata )
+void CEnvFade::InputFade(inputdata_t &inputdata)
 {
 	int fadeFlags = 0;
 
-	if ( m_spawnflags & SF_FADE_IN )
+	if (m_spawnflags & SF_FADE_IN)
 	{
 		fadeFlags |= FFADE_IN;
 	}
@@ -83,29 +90,82 @@ void CEnvFade::InputFade( inputdata_t &inputdata )
 		fadeFlags |= FFADE_OUT;
 	}
 
-	if ( m_spawnflags & SF_FADE_MODULATE )
+	if (m_spawnflags & SF_FADE_MODULATE)
 	{
 		fadeFlags |= FFADE_MODULATE;
 	}
 
-	if ( m_spawnflags & SF_FADE_STAYOUT )
+	if (m_spawnflags & SF_FADE_STAYOUT)
 	{
 		fadeFlags |= FFADE_STAYOUT;
 	}
 
-	if ( m_spawnflags & SF_FADE_ONLYONE )
+	if (m_spawnflags & SF_FADE_ONLYONE)
 	{
-		if ( inputdata.pActivator && inputdata.pActivator->IsNetClient() )
+#ifdef INFESTED_DLL
+		if (inputdata.pActivator->Classify() == CLASS_ASW_MARINE)
 		{
-			UTIL_ScreenFade( inputdata.pActivator, m_clrRender, Duration(), HoldTime(), fadeFlags );
+			CASW_Marine *pMarine = static_cast<CASW_Marine*>(inputdata.pActivator);
+			CASW_Player *pPlayer = pMarine->GetCommander();
+			if (pPlayer && pMarine->IsInhabited())
+			{
+				UTIL_ScreenFade(pPlayer, m_clrRender, Duration(), HoldTime(), fadeFlags);
+			}
+		}
+#else
+		if (inputdata.pActivator->IsNetClient())
+		{
+			UTIL_ScreenFade(inputdata.pActivator, m_clrRender, Duration(), HoldTime(), fadeFlags);
+		}
+#endif
+	}
+	else
+	{
+		UTIL_ScreenFadeAll(m_clrRender, Duration(), HoldTime(), fadeFlags | FFADE_PURGE);
+	}
+
+	m_OnBeginFade.FireOutput(inputdata.pActivator, this);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Input that does the exact opposite of the Fade input
+//-----------------------------------------------------------------------------
+void CEnvFade::InputFadeReverse(inputdata_t &inputdata)
+{
+	int fadeFlags = 0;
+
+	if (m_spawnflags & SF_FADE_IN)
+	{
+		fadeFlags |= FFADE_OUT;
+	}
+	else
+	{
+		fadeFlags |= FFADE_IN;
+	}
+
+	if (m_spawnflags & SF_FADE_MODULATE)
+	{
+		fadeFlags |= FFADE_MODULATE;
+	}
+
+	if (m_spawnflags & SF_FADE_STAYOUT)
+	{
+		fadeFlags |= FFADE_STAYOUT;
+	}
+
+	if (m_spawnflags & SF_FADE_ONLYONE)
+	{
+		if (inputdata.pActivator->IsNetClient())
+		{
+			UTIL_ScreenFade(inputdata.pActivator, m_clrRender, m_ReverseDuration, HoldTime(), fadeFlags);
 		}
 	}
 	else
 	{
-		UTIL_ScreenFadeAll( m_clrRender, Duration(), HoldTime(), fadeFlags|FFADE_PURGE );
+		UTIL_ScreenFadeAll(m_clrRender, m_ReverseDuration, HoldTime(), fadeFlags | FFADE_PURGE);
 	}
 
-	m_OnBeginFade.FireOutput( inputdata.pActivator, this );
+	m_OnBeginFade.FireOutput(inputdata.pActivator, this);
 }
 
 
@@ -115,29 +175,29 @@ void CEnvFade::InputFade( inputdata_t &inputdata )
 // Input  : flTime - Returns the fade time in seconds (the time to fade in or out)
 //			clrFade - Returns the color to fade to or from.
 //-----------------------------------------------------------------------------
-static void GetFadeParms( const CCommand &args, float &flTime, color32 &clrFade)
+static void GetFadeParms(const CCommand &args, float &flTime, color32 &clrFade)
 {
 	flTime = 2.0f;
 
-	if ( args.ArgC() > 1 )
+	if (args.ArgC() > 1)
 	{
-		flTime = atof( args[1] );
+		flTime = atof(args[1]);
 	}
-	
+
 	clrFade.r = 0;
 	clrFade.g = 0;
 	clrFade.b = 0;
 	clrFade.a = 255;
 
-	if ( args.ArgC() > 4 )
+	if (args.ArgC() > 4)
 	{
-		clrFade.r = atoi( args[2] );
-		clrFade.g = atoi( args[3] );
-		clrFade.b = atoi( args[4] );
+		clrFade.r = atoi(args[2]);
+		clrFade.g = atoi(args[3]);
+		clrFade.b = atoi(args[4]);
 
-		if ( args.ArgC() == 5 )
+		if (args.ArgC() == 5)
 		{
-			clrFade.a = atoi( args[5] );
+			clrFade.a = atoi(args[5]);
 		}
 	}
 }
@@ -146,55 +206,61 @@ static void GetFadeParms( const CCommand &args, float &flTime, color32 &clrFade)
 //-----------------------------------------------------------------------------
 // Purpose: Console command to fade out to a given color.
 //-----------------------------------------------------------------------------
-static void CC_FadeOut( const CCommand &args )
+static void CC_FadeOut(const CCommand &args)
 {
 	float flTime;
 	color32 clrFade;
-	GetFadeParms( args, flTime, clrFade );
+	GetFadeParms(args, flTime, clrFade);
 
 	CBasePlayer *pPlayer = UTIL_GetCommandClient();
-	UTIL_ScreenFade( pPlayer, clrFade, flTime, 0, FFADE_OUT | FFADE_PURGE | FFADE_STAYOUT );
+	UTIL_ScreenFade(pPlayer, clrFade, flTime, 0, FFADE_OUT | FFADE_PURGE | FFADE_STAYOUT);
 }
-static ConCommand fadeout("fadeout", CC_FadeOut, "fadeout {time r g b}: Fades the screen to black or to the specified color over the given number of seconds.", FCVAR_CHEAT );
+static ConCommand fadeout("fadeout", CC_FadeOut, "fadeout {time r g b}: Fades the screen to black or to the specified color over the given number of seconds.", FCVAR_CHEAT);
 
 
 //-----------------------------------------------------------------------------
 // Purpose: Console command to fade in from a given color.
 //-----------------------------------------------------------------------------
-static void CC_FadeIn( const CCommand &args )
+static void CC_FadeIn(const CCommand &args)
 {
 	float flTime;
 	color32 clrFade;
-	GetFadeParms( args, flTime, clrFade );
+	GetFadeParms(args, flTime, clrFade);
 
 	CBasePlayer *pPlayer = UTIL_GetCommandClient();
-	UTIL_ScreenFade( pPlayer, clrFade, flTime, 0, FFADE_IN | FFADE_PURGE );
+	UTIL_ScreenFade(pPlayer, clrFade, flTime, 0, FFADE_IN | FFADE_PURGE);
 }
 
-static ConCommand fadein("fadein", CC_FadeIn, "fadein {time r g b}: Fades the screen in from black or from the specified color over the given number of seconds.", FCVAR_CHEAT );
+static ConCommand fadein("fadein", CC_FadeIn, "fadein {time r g b}: Fades the screen in from black or from the specified color over the given number of seconds.", FCVAR_CHEAT);
 
 
 //-----------------------------------------------------------------------------
 // Purpose: Draw any debug text overlays
 // Output : Current text offset from the top
 //-----------------------------------------------------------------------------
-int CEnvFade::DrawDebugTextOverlays( void ) 
+int CEnvFade::DrawDebugTextOverlays(void)
 {
 	int text_offset = BaseClass::DrawDebugTextOverlays();
 
-	if (m_debugOverlays & OVERLAY_TEXT_BIT) 
+	if (m_debugOverlays & OVERLAY_TEXT_BIT)
 	{
 		char tempstr[512];
 
 		// print duration
-		Q_snprintf(tempstr,sizeof(tempstr),"    duration: %f", m_Duration);
-		EntityText(text_offset,tempstr,0);
+		Q_snprintf(tempstr, sizeof(tempstr), "    duration: %f", m_Duration);
+		EntityText(text_offset, tempstr, 0);
+		text_offset++;
+
+		// print reverse duration
+		Q_snprintf(tempstr, sizeof(tempstr), "    reverse duration: %f", m_ReverseDuration);
+		EntityText(text_offset, tempstr, 0);
 		text_offset++;
 
 		// print hold time
-		Q_snprintf(tempstr,sizeof(tempstr),"    hold time: %f", m_HoldTime);
-		EntityText(text_offset,tempstr,0);
+		Q_snprintf(tempstr, sizeof(tempstr), "    hold time: %f", m_HoldTime);
+		EntityText(text_offset, tempstr, 0);
 		text_offset++;
+
 	}
 	return text_offset;
 }
